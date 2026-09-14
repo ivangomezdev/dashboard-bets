0bf7ed7e13f271505c53828b4483b3b0c7299fcc
ivan <ivansangomez6@gmail.com>
2026-09-14 09:02:51 -0700

    Hide internal reason comments from arb details

--- components/ArbsTable.js
+++ components/ArbsTable.js
@@ -125,11 +125,6 @@ export default function ArbsTable({ arbs }) {
                             </small>
                           </div>
                         ))}
-                        {arb.reason && !/^verificacion periodica/i.test(arb.reason) ? (
-                          <div className="arb-reason">
-                            <strong>Motivo:</strong> {arb.reason}
-                          </div>
-                        ) : null}
                       </div>
                     </details>
                   </td>
